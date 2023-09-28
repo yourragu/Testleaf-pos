91b95995bfcfbfb5887e5e530c3d8653838f9d3c
Testleaf <mohan.balakrishnan23@gmail.com>
2023-09-28 16:42:25 +0530

    Update apiBuilder.js

--- src/api/apiBuilder.js
+++ src/api/apiBuilder.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const URL_PREFIX = process.env.REACT_APP_IS_PROD ?
   process.env.REACT_APP_PROD_URL_PREFIX :
-  "http://98.70.2.21:3500/api";
+  "http://20.40.56.250:3500/api";
 
 const apiBuilder = mainRouteName => ({
   fetchById: id => axios.get(`${URL_PREFIX}/${mainRouteName}/${id}`),
